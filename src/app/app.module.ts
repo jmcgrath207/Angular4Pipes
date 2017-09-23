b434ba7d8c6c406397abd71e9892a417df07b2cf
sonance207 <sonance207@gmail.com>
2017-09-23 16:41:42 -0500

    making custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ShortPipe} from './short.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortPipe
   ],
   imports: [
     BrowserModule,
